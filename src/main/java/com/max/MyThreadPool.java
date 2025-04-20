9605d3374c3c47f7a94691f0e6230b119d2321fa
bobobbbb <1665052137@qq.com>
2025-04-20 18:48:35 +0800

    简单实现浪费资源的线程复用

--- src/main/java/com/max/MyThreadPool.java
+++ src/main/java/com/max/MyThreadPool.java
@@ -1,7 +1,20 @@
 package com.max;
 
-public class MyThreadPool {
-    void execute(Runnable command){
+import java.util.ArrayList;
+import java.util.List;
 
+public class MyThreadPool {
+    List<Runnable> commandList=new ArrayList<>();
+
+    Thread thread=new Thread(()->{
+        while(true){
+            commandList.isEmpty();
+            Runnable command=commandList.remove(0) ;
+            command.run();
+        }
+    });
+
+    void execute(Runnable command){
+        commandList.add(command);
     }
 }
