d6c603369b561607ef5bf000dd1123388d2015df
bobobbbb <1665052137@qq.com>
2025-04-21 15:23:45 +0800

    实现核心线程+非核心线程的队列实现

--- src/main/java/com/max/MyThreadPool.java
+++ src/main/java/com/max/MyThreadPool.java
@@ -1,5 +1,7 @@
 package com.max;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
@@ -11,8 +13,7 @@ public class MyThreadPool {
     // 线程安全的阻塞队列，容量为1024
     BlockingQueue<Runnable> blockingQueue = new ArrayBlockingQueue<>(1024);
 
-    // 工作线程，不断从队列中取出任务执行
-    Thread thread = new Thread(() -> {
+    private final Runnable task=() -> {
         while (true) {
             try {
                 // 阻塞式获取任务，没有任务时会等待，不会空转
@@ -25,19 +26,45 @@ public class MyThreadPool {
                 break;
             }
         }
-    },"唯一线程");
+    };
+    //核心的线程数默认为10
+    private int corePoolSize=10;
 
-    // 构造方法中启动线程
-    public MyThreadPool() {
-        thread.start(); // 一定要启动线程
-    }
+    //最大线程容量
+    private int maxSize=16;
+
+    //线程集合
+    List<Thread> coreList=new ArrayList<>();
+
+    //支持线程
+    List<Thread> supportList=new ArrayList<>();
+
 
     /**
      * 将一个任务提交给线程池执行
      */
     public void execute(Runnable command) {
-        // 添加任务到队列中（注意：add在队列满时会抛异常，可用put阻塞或offer非阻塞）
-        boolean offer = blockingQueue.offer(command);
-
+        //我们判断这个list中有多少个线程，如果线程数小于core就创建一个线程
+        if(coreList.size()<corePoolSize){
+            //创建一个线程
+            Thread thread = new Thread(task);
+            //添加进线程中
+            coreList.add(thread);
+            //执行命令
+            thread.start();
+        }
+        //如果大于核心线程数，我们就放入阻塞队列
+        if(blockingQueue.offer(command)){
+            return;
+        }
+        //如果队列满了，就新建线程来完成任务
+        if(coreList.size()+supportList.size()<maxSize){
+            Thread thread = new Thread(task);
+            supportList.add(thread);
+            thread.start();
+        }
+        if(!blockingQueue.offer(command)){
+            throw new RuntimeException("阻塞队列满了");
+        }
     }
 }
